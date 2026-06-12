10897af95be6c1b4673516536bb45319c4c40151
Divyansh Upadhyay <24051547@kiit.ac.in>
2026-06-12 14:29:16 +0530

    Update javaCode1.java

--- javaCode1.java
+++ javaCode1.java
@@ -1,5 +1,6 @@
 public class javaCode1 {
   public static void main(String[] args){
       System.out.println("Hello this is third time i am printing HELLO");
+      System.out.println("hey!! its me again...... :)");
   }
 }
